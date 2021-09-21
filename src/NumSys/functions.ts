73fd4052c179668df7cd43f04d460460db1f9afc
Rhseung <56152093+Rhseung@users.noreply.github.com>
2021-09-21 23:24:04 +0900

    Update functions.ts

--- src/NumSys/functions.ts
+++ src/NumSys/functions.ts
@@ -2,6 +2,18 @@ import { Z } from 'Z';
 import { Q } from 'Q';
 import { R } from 'R';
 
-export function add (...nums: (Z[] | Q[] | R[])) {
+export function add (...nums: (Z | Q | R)[]) {
+
+}
+
+export function sub (...nums: (Z | Q | R)[]) {
+
+}
+
+export function mul (...nums: (Z | Q | R)[]) {
+
+}
+
+export function div (...nums: (Z | Q | R)[]) {
 
 }
