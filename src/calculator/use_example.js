3cdc6e09cb56d654b8161bc606b570c89383f756
Rhseung <56152093+Rhseung@users.noreply.github.com>
2021-10-03 13:20:26 +0900

    Update use_example.js

--- src/calculator/use_example.js
+++ src/calculator/use_example.js
@@ -1,4 +1,12 @@
+const { add } = require("../types/F");
+
 f = new Formula("1 + 2 * 3 - 4");
 f.toPostFixNotation() // -> [1, 2, 3, mul, add, 4, sub]
+f.oper = {
+  'add': {
+    shape: '+',
+    execute: (numbers) => add(numbers)
+  },
+}
 
 f.calculate() // -> 3
